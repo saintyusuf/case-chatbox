32b0cad0daa216c3da49b6c791bc34cc3120fa3f
yusufcode <yusuf1akbaba@gmail.com>
2021-06-10 19:41:05 +0300

    remove message alert update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(document).on('click', '.message-box-button', function() {
 
     if(!$('.message-box').hasClass('show')){
         $('.message-box').fadeIn(200).addClass('show')
+        $('.message-box-button').find('.message-alert').remove()
     } else {
         $('.message-box').fadeOut(200).removeClass('show')
     }
